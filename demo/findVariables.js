b602f505c22fa13bac474812c8385dc388489f91
Kee-Yip Chan <keeyipchan@gmail.com>
2013-01-20 19:44:00 -0800

    Improve variable-scope lookup. Improve coloring for usability

--- demo/findVariables.js
+++ demo/findVariables.js
@@ -30,19 +30,19 @@
 		].join(','),
 		hoistedBlockTypes = /^(?:For|If|While|Do)Statement$/,
 		lookupScope = $.map(varLookup, function(xs, type) { return hoistedBlockTypes.test(type) ? null : 'es.' + type }).join(',').replace(/(^,)|(,,)|(,$)/g, ''),
-		scopeTypes = $.map(varLookup, function(xs, type) { return 'es.' + type }).join(','),
+		blockTypes = $.map(varLookup, function(xs, type) { return 'es.' + type }).join(','),
 		cachedVarLookup = {}
 
 	$.each(varLookup, function(type,xs) {
 		cachedVarLookup[type] = xs.join(',')
 	})
 
-	function getVar($referenceScope, name, lookup) {
-		if (!$referenceScope.length)
+	function getVar($idBlock, name, lookup) {
+		if (!$idBlock.length)
 			return $()
 
-		var type = $referenceScope.data('type'),
-			$var = $referenceScope.find(lookup.replace(/{{name}}/g, name))
+		var type = $idBlock.data('type'),
+			$var = $idBlock.find(lookup.replace(/{{name}}/g, name))
 
 		return $var
 	}
@@ -60,8 +60,11 @@
 	function findVariables($ast) {
 
 		// Pass 1, identify: .scope, assign data-scope-id
-		var globalScopeId = 0
-		// TODO: Extract from Pass 2
+		$ast.find(lookupScope).each(function(i) {
+			$(this)
+				.addClass('scope')
+				.attr('data-scope-id', i+1)
+		})
 
 		// Pass 2, identify: .local-variable, .upper-variable
 		$ast.find('es.Identifier').each(function() {
@@ -70,21 +73,21 @@
 			if (isObjectKey($id))
 				return
 
-			var $referenceScope = $id.closest(scopeTypes),
-				scopeType = $referenceScope.data('type'),
-				idName = $id.data('name'),
-				$var = getVar($referenceScope, idName, cachedVarLookup[scopeType])
+			var $idBlock = $id.closest(blockTypes),
+				idBlockType = $idBlock.data('type'),
+				name = $id.data('name'),
+				$var = getVar($idBlock, name, cachedVarLookup[idBlockType])
 
-			while (!$var.length && hoistedBlockTypes.test(scopeType)) {
-				$var = getVar($referenceScope, idName, cachedVarLookupForHoisted)
+			while (!$var.length && hoistedBlockTypes.test(idBlockType)) {
+				$var = getVar($idBlock, name, cachedVarLookupForHoisted)
 				if (!$var.length) {
-					$referenceScope = $referenceScope.closest('es.BlockStatement').parent()
-					scopeType = $referenceScope.data('type')
-					if (!hoistedBlockTypes.test(scopeType)) {
-						$var = getVar($referenceScope, idName, cachedVarLookup[scopeType])
+					$idBlock = $idBlock.closest('es.BlockStatement').parent()
+					idBlockType = $idBlock.data('type')
+					if (!hoistedBlockTypes.test(idBlockType)) {
+						$var = getVar($idBlock, name, cachedVarLookup[idBlockType])
 						break;
-					} else if (scopeType === 'ForStatement') {
-						$var = getVar($referenceScope, idName, cachedVarLookup[scopeType])
+					} else if (idBlockType === 'ForStatement') {
+						$var = getVar($idBlock, name, cachedVarLookup[idBlockType])
 					}
 				}
 			}
@@ -92,41 +95,29 @@
 			if ($var.length) {
 				// Resolve .local-variable
 
-				var $scope = $var.data('scope')
+				var $scope = $var.data('scope'),
+					scopeId
 				if (!$scope) {
-					if (scopeType === 'FunctionDeclaration' || hoistedBlockTypes.test(scopeType)) {
-						$scope = $referenceScope.parent().closest(lookupScope)
-					} else {
-						$scope = $referenceScope
-					}
-					$var.data('scope', $scope.addClass('scope'))
+					$scope = $var.closest('es.scope')
+					if ($scope.data('type') === 'FunctionDeclaration' && $scope.find('> es.id').is($var))
+						$scope = $var.closest('es.scope').parent().closest('es.scope')
+					$var.data('scope', $scope)
 				}
-				var scopeId = $scope.attr('data-scope-id')
-				if (!scopeId)
-					$scope.attr('data-scope-id', scopeId = ++globalScopeId)
-
-				/*
-				var varLocator = '[data-scope-id="' + scopeId + '"][name="' + $id.data('name') + '"]'
-
-				if ($scope.find(varLocator).length === 0) {
-					$scope.on('click', varLocator, function() {
-						$('.highlighted-variable').removeClass('highlighted-variable')
-						$scope.find(varLocator).addClass('highlighted-variable')
-					})
-				}
-				*/
+				scopeId = $scope.data('scope-id')
 
 				$id
-					.addClass('local-variable')
 					.attr('data-scope-id', scopeId)
+					.addClass(scopeId === 1 ? 'global-variable' : 'local-variable')
 
 				$var
 					.addClass('declared')
 					.attr('data-scope-id', scopeId)
 					.attr('data-count', ($var.data('count') || 0) + 1)
+					.attr('title', 'Local: ' + $var.data('count') + ' references')
 
 				if (!$id.is($var)) {
-					$id.addClass('reference')
+					$id
+						.addClass('reference')
 				}
 			} else {
 				$id.addClass('reference upper-variable')
@@ -135,29 +126,56 @@
 
 		// Pass 3, resolve .upper-variable
 		$ast.find('.reference.upper-variable').each(function() {
-			var $ref = $(this),
+			var $id = $(this),
 				// Start from 2nd closest parent scope (grandfather)
-				$scope = $ref.closest('.scope').parent().closest('.scope'),
-				name = $ref.data('name')
+				$scope = $id.closest('.scope').parent().closest('.scope'),
+				name = $id.data('name')
 
 			for (var $upperVar = $(), varLocator; $scope.length && !$upperVar.length; $scope = $scope.parent().closest('.scope')) {
 				varLocator = '.declared[data-scope-id="' + $scope.data('scope-id') + '"][name="' + name + '"]'
-				console.log('locator',varLocator)
 				$upperVar = $scope.find(varLocator)
 			}
 
 			if ($upperVar.length) {
-				$ref
-					.attr('data-scope-id', $upperVar.data('scope-id'))
+				var scopeId = $upperVar.data('scope-id')
+				$id
+					.attr('data-scope-id', scopeId)
+
+				if (scopeId === 1)
+					$id.addClass('global-variable')
+				else
+					$id.addClass('shared-variable')
 
 				$upperVar
-					.attr('data-count', ($upperVar.data('count') || 0) + 1)
+					.attr('data-count', $upperVar.data('count') + 1)
+					.attr('title', (scopeId === 1 ? 'Global: ' : 'Shared: ') + $upperVar.data('count') + ' references')
 			} else {
-				$ref
-					.addClass('undefined')
+				$id
+					.addClass('undefined-variable global-variable')
 					.attr('data-scope-id', 1)
 			}
 		})
+
+		// Pass 4, resolve global assignments
+		// TODO: Resolve WithStatements
+		$ast.find('es.AssignmentExpression > es.left.undefined-variable').each(function() {
+			var $id = $(this),
+				name = $id.data('name')
+
+			if (!$id.hasClass('undefined-variable'))
+				return
+
+			var $references = $ast.find('es.undefined-variable[data-scope-id="1"][name="' + name + '"]')
+				.removeClass('undefined-variable')
+		})
+
+		// Pass 5, resolve js builtins
+		var builtinsLookup = $.map(['Date','Array','Math'], function(name) { return 'es.undefined-variable[name="' + name + '"]' }).join(',')
+		$ast.find(builtinsLookup).removeClass('undefined-variable').addClass('js-builtin')
+
+		// Pass 6, resolve browser builtins
+		var browserBuiltinsLookup = $.map(['window','setInterval', 'setTimeout','alert','console','document', 'history'], function(name) { return 'es.undefined-variable[name="' + name + '"]' }).join(',')
+		$ast.find(browserBuiltinsLookup).removeClass('undefined-variable').addClass('browser-builtin')
 	}
 
 	window.findVariables = findVariables
